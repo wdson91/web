0f1c39ab007b572cef676702019bd20131a58c04
Wdson Danrlei <44691537+wdson91@users.noreply.github.com>
2024-02-22 16:59:14 -0300

    Update index.py

--- index.py
+++ index.py
@@ -5,28 +5,26 @@ from datetime import datetime
 import pytz
 
 async def executar_ambos(hour):
-    while True:
-        await asyncio.gather(
-            main_voupra(hour,run_once=True),
-            main_vmz(hour,run_once=True),
-            main_ml(hour, run_once=True)
-        )
-        logging.info("Aguardando a próxima execução...")
-        
+    await asyncio.gather(
+        main_voupra(hour, run_once=True),
+        main_vmz(hour, run_once=True),
+        main_ml(hour, run_once=True)
+    )
+    logging.info("Aguardando a próxima execução...")
 
 async def agendar_execucao():
     current_time = datetime.now(pytz.timezone('America/Sao_Paulo'))
-
-    target_hours = ["7:00","11:00", "14:00", "17:00"]  # Horas desejadas como strings no formato "HH:MM"
+    target_hours = ["7:00","11:00", "16:28", "17:00"]
 
     for hour in target_hours:
         if current_time.strftime("%H:%M") == hour:
             await executar_ambos(hour)
+            break  # Uma vez que a tarefa é executada, não precisamos continuar verificando os outros horários
 
 async def main():
     while True:
-        await agendar_execucao()  # Agendamento das tarefas dentro do loop principal
-        await asyncio.sleep(60)  # Verifica o schedule a cada minuto
+        await agendar_execucao()
+        await asyncio.sleep(60)
 
 if __name__ == "__main__":
     asyncio.run(main())
